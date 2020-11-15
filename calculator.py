619cde22e8d784e9c0e2a3d71846ec188d18829a
todeus <gtodeus@gmail.com>
2020-11-15 13:10:18 +1000

    Stage 4/7: Add subtractions
    The program must calculate expressions like these: 4 + 6 - 8, 2 - 3 - 4, and so on.
    Modify the result of the /help command to explain these operations.
    Decompose your program using functions to make it easy to understand and edit later.
    The program should not stop until the user enters the /exit command.
    If you encounter an empty line, do not output anything.

--- calculator.py
+++ calculator.py
@@ -1,10 +1,26 @@
 # write your code here
 
+def determ_op(string):
+    if "+" in string:
+        return 1
+    elif string.count("-") % 2 == 0:
+        return 1
+    else:
+        return -1
+
+
 def result(string):
-    num = [int(x) for x in string.split()]
-    if len(num) == 0:
+    ops = string.split()
+    op = 1
+    summ = 0
+    if len(ops) == 0:
         return
-    print(sum(num))
+    for o in ops:
+        try:
+            summ += int(o) * op
+        except ValueError:
+            op = determ_op(o)
+    print(summ)
 
 
 def main():
@@ -15,6 +31,7 @@ def main():
             break
         elif string == "/help":
             print("The program calculates the sum of numbers")
+            print("The program calculate expressions like these: 4 + 6 - 8, 2 - 3 - 4, and so on.")
         else:
             result(string)
 
