7f80d0e9407a5a22de08a2ca59a76ea0723a1f08
todeus <gtodeus@gmail.com>
2020-11-16 12:37:54 +1000

    Stage 5/7: Error_
    The program should print "Invalid expression" in cases when the given expression has an invalid format. If a user enters an invalid command, the program must print "Unknown command". All messages must be printed without quotes. The program must never throw an exception.
    To handle incorrect input, you should remember that the user input that starts with "/" is a command, in other situations it is an expression.
    
    Do not forget to write methods to decompose your program.
    
    Like before, /help command should print information about your program. When the command /exit is entered, the program must print "Bye!" (without quotes), and then stop.

--- calculator.py
+++ calculator.py
@@ -1,39 +1,83 @@
 # write your code here
+from collections import deque
 
-def determ_op(string):
-    if "+" in string:
-        return 1
-    elif string.count("-") % 2 == 0:
-        return 1
+
+def split_to_deque(string):
+    deq = deque()
+    op = string.split()
+
+    for x in op:
+        deq.append(x)
+
+    return deq
+
+
+def operate(x, y, op):
+    if determinate_operation(op) == "+":
+        return int(x) + int(y)
+    elif determinate_operation(op) == "-":
+        return int(x) - int(y)
     else:
-        return -1
+        return "Invalid expression"
 
 
-def result(string):
-    ops = string.split()
-    op = 1
-    summ = 0
-    if len(ops) == 0:
-        return
-    for o in ops:
+def determinate_operation(op):
+    if op == "+":
+        return "+"
+    elif op == "-":
+        return "-"
+    elif len(op) > 1 and (op.count("-") == 1 or op.count("+") == 1):
+        return None
+    elif len(op) > 1:
+        op = op.replace("--", "+")
+        op = op.replace("++", "+")
+        op = op.replace("+-", "-")
+        op = op.replace("-+", "-")
+        op = determinate_operation(op)
+        return op
+
+
+def recurent_calc(a):
+    if len(a) == 1:
+        return int(a[0])
+    if len(a) == 3:
+        return operate(a[0], a[2], a[1])
+    x = a.popleft()
+    op = determinate_operation(a.popleft())
+    y = a.popleft()
+    a.appendleft(operate(x, y, op))
+
+    return recurent_calc(a)
+
+
+def calculate(a):
+    deq = split_to_deque(a)
+    if len(deq) % 2 == 0:
+        return "Invalid expression"
+    else:
         try:
-            summ += int(o) * op
+            return recurent_calc(deq)
         except ValueError:
-            op = determ_op(o)
-    print(summ)
+            return "Invalid expression"
 
 
 def main():
     while True:
         string = input()
+        if string == "":
+            continue
         if string == "/exit":
             print("Bye!")
             break
         elif string == "/help":
             print("The program calculates the sum of numbers")
             print("The program calculate expressions like these: 4 + 6 - 8, 2 - 3 - 4, and so on.")
+        elif string.startswith("/"):
+            print("Unknown command")
         else:
-            result(string)
+            print(calculate(string))
+
+
 
 
 if __name__ == "__main__":
